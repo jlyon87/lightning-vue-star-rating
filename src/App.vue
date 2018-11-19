1352dca8f51a7aa5b7bfc671c903f8cb76d0da4f
Justin Lyon <jml6487@gmail.com>
2018-11-19 13:39:09 -0600

    boilerplating in progress

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="container">
+    Testing
+  </div>
+</template>
+
+<script>
+import StarRating from 'vue-star-rating'
+
+export default {
+  components: {
+    StarRating
+  }
+}
+</script>
