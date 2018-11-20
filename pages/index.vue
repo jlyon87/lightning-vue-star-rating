459c5ad0a61e057b3fe884dfea9f3dbbdb6c1ad3
Justin Lyon <jml6487@gmail.com>
2018-11-20 07:38:22 -0600

    add lightning-container

--- pages/index.vue
+++ pages/index.vue
@@ -5,9 +5,16 @@
 </template>
 
 <script>
+import LCC from 'lightning-container'
 import StarRating from 'vue-star-rating'
 
 export default {
+  mounted() {
+    LCC.sendMessage('starRating mounted')
+    LCC.addMessageHandler(message => {
+      console.log('message received in star rating')
+    })
+  },
   components: {
     StarRating
   }
